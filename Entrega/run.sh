07b4ebaf9b6093e0cb0b286b73b2d5bf71db8a9f
unknown <miguelmendescoelho@gmail.com>
2019-11-15 20:21:55 +0000

    Fixed IF verb, Added final folder

--- Entrega/run.sh
+++ Entrega/run.sh
@@ -3,40 +3,42 @@
 ################### TRANSDUCERS a)################
 #
 #a1
-fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2noun.txt | fstarcsort > lemma2noun.fst
-fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2noun.fst | dot -Tpdf  > lemma2noun.pdf
+fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2noun.txt | fstarcsort > FINALtransducers/lemma2noun.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2noun.fst | dot -Tpdf  > FINALpdf/lemma2noun.pdf
 
 #a2
-fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2adverb.txt | fstarcsort > lemma2adverb.fst
-fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2adverb.fst | dot -Tpdf  > lemma2adverb.pdf
+fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2adverb.txt | fstarcsort > FINALtransducers/lemma2adverb.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2adverb.fst | dot -Tpdf  > FINALpdf/lemma2adverb.pdf
 
 #a3
-fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2verbip.txt | fstarcsort > lemma2verbip.fst
-fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2verbip.fst | dot -Tpdf  > lemma2verbip.pdf
+fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2verbip.txt | fstarcsort > FINALtransducers/lemma2verbip.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2verbip.fst | dot -Tpdf  > FINALpdf/lemma2verbip.pdf
 
 #a4
-fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2verbis.txt | fstarcsort > lemma2verbis.fst
-fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2verbis.fst | dot -Tpdf  > lemma2verbis.pdf
+fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2verbis.txt | fstarcsort > FINALtransducers/lemma2verbis.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2verbis.fst | dot -Tpdf  > FINALpdf/lemma2verbis.pdf
 
 #a5
-fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2verbif.txt | fstarcsort > lemma2verbif.fst
-fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2verbif.fst | dot -Tpdf  > lemma2verbif.pdf
+fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2verbif.txt | fstarcsort > FINALtransducers/lemma2verbif.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2verbif.fst | dot -Tpdf  > FINALpdf/lemma2verbif.pdf
 
 #b6
 fstunion lemma2verbip.fst lemma2verbis.fst > lemma2verb1.fst
-fstunion lemma2verb1.fst lemma2verbif.fst > lemma2verb.fst
+fstunion lemma2verb1.fst lemma2verbif.fst > FINALtransducers/lemma2verb.fst
+rm -f lemma2verb1.fst
 
-fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2verb.fst | dot -Tpdf  > lemma2verb.pdf
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2verb.fst | dot -Tpdf  > FINALpdf/lemma2verb.pdf
 
 #b7
 fstunion lemma2noun.fst lemma2adverb.fst > lemma2word1.fst
-fstunion lemma2word1.fst lemma2verb.fst > lemma2word.fst
+fstunion lemma2word1.fst lemma2verb.fst > FINALtransducers/lemma2word.fst
+rm -f lemma2word1.fst
 
-fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2word.fst | dot -Tpdf  > lemma2word.pdf
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2word.fst | dot -Tpdf  > FINALpdf/lemma2word.pdf
 
 #c8
-fstinvert lemma2word.fst > word2lemma.fst
-fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait word2lemma.fst | dot -Tpdf  > word2lemma.pdf
+fstinvert lemma2word.fst > FINALtransducers/word2lemma.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait word2lemma.fst | dot -Tpdf  > FINALpdf/word2lemma.pdf
 
 
 ################### Testes ################
